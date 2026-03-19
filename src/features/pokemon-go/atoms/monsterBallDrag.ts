801f8053d62c25e427a4b4e58209c2d37a5c877e
mecaota <24618982+mecaota@users.noreply.github.com>
2026-03-19 15:57:55 +0900

    雑実装

--- src/features/pokemon-go/atoms/monsterBallDrag.ts
+++ src/features/pokemon-go/atoms/monsterBallDrag.ts
@@ -1,57 +1,77 @@
-function setupMonsterBallDrag() {
-  const pokeballButton = document.querySelector('.pokeball-button');
-  if (!(pokeballButton instanceof HTMLElement)) {
-    return;
+let isBallDragging = false;
+
+const getPokemonBallElement = () => {
+  const element = document.querySelector('.pokeball-ball');
+  if (!(element instanceof HTMLElement)) {
+    console.error('Pokeball element not found');
   }
+  return element as HTMLElement | null;
+};
+const getPokemonGoAreaElement = () => {
+  const element = document.getElementById('go-pokemon');
+  if (!(element instanceof HTMLElement)) {
+    console.error('Pokemon Go area element not found');
+  }
+  return element as HTMLElement | null;
+};
+const getGochaTargetElement = () => {
+  const element = document.getElementById('gocha-target');
+  if (!(element instanceof HTMLElement)) {
+    console.error('Gocha target element not found');
+  }
+  return element as HTMLElement | null;
+};
 
-  pokeballButton.addEventListener('dragstart', onDragStartHandler());
-  pokeballButton.addEventListener('mousedown', onMouseDownHandler());
-  pokeballButton.addEventListener('mousemove', onMouseMoveHandler());
-  pokeballButton.addEventListener('mouseup', onMouseUpHandler());
-}
+function setupMonsterBallDrag() {
+  const pokeball = getPokemonBallElement();
 
-function onDragStartHandler() {
-  return (e: DragEvent) => {
-    if (!e.dataTransfer) {
-      return;
-    }
-    e.dataTransfer.setData('text/plain', 'pokeball');
-  };
+  pokeball?.addEventListener('mousedown', onMouseDownHandler);
+  document.addEventListener('mousemove', onMouseMoveHandler);
+  document.addEventListener('mouseup', onMouseUpHandler);
 }
 
 function onMouseDownHandler() {
-  return (e: MouseEvent) => {
-    const pokeballButton = e.currentTarget;
-    if (!(pokeballButton instanceof HTMLElement)) {
-      return;
-    }
-    pokeballButton.style.position = 'absolute';
-    pokeballButton.style.left = `${e.pageX - pokeballButton.offsetWidth / 2}px`;
-    pokeballButton.style.top = `${e.pageY - pokeballButton.offsetHeight / 2}px`;
-  };
+  isBallDragging = true;
 }
-
-function onMouseMoveHandler() {
-  return (e: MouseEvent) => {
-    const pokeballButton = e.currentTarget;
-    if (!(pokeballButton instanceof HTMLElement)) {
-      return;
-    }
-    pokeballButton.style.left = `${e.pageX - pokeballButton.offsetWidth / 2}px`;
-    pokeballButton.style.top = `${e.pageY - pokeballButton.offsetHeight / 2}px`;
-  };
-}
-
 function onMouseUpHandler() {
-  return (e: MouseEvent) => {
-    const pokeballButton = e.currentTarget;
-    if (!(pokeballButton instanceof HTMLElement)) {
-      return;
-    }
-    pokeballButton.style.position = '';
-    pokeballButton.style.left = '';
-    pokeballButton.style.top = '';
-  };
+  isBallDragging = false;
 }
+function onMouseMoveHandler(e: MouseEvent) {
+  if (!isBallDragging) {
+    return;
+  }
+  const pokeballButton = getPokemonBallElement();
+  const pokeballDragArea = getPokemonGoAreaElement();
+  const gochaTarget = getGochaTargetElement();
+  const cursorX = e?.pageX;
+  const cursorY = e?.pageY;
+  if (
+    !(pokeballButton && pokeballDragArea && gochaTarget) ||
+    cursorX === undefined ||
+    cursorY === undefined
+  ) {
+    return;
+  }
+  // console.log('Dragging pokeball to:', cursorX, cursorY);
+  pokeballButton.style.left = `${cursorX - pokeballDragArea.offsetLeft - pokeballButton.offsetWidth / 2}px`;
+  pokeballButton.style.top = `${cursorY - pokeballDragArea.offsetTop - pokeballButton.offsetHeight / 2}px`;
+
+  const isIntersecting = checkRectIntersection(
+    gochaTarget?.getBoundingClientRect(),
+    pokeballButton.getBoundingClientRect(),
+  );
+  if (isIntersecting) {
+    gochaTarget.classList.add('gocha');
+  }
+}
+
+const checkRectIntersection = (rect1: DOMRect, rect2: DOMRect) => {
+  return !(
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right ||
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom
+  );
+};
 
 export { setupMonsterBallDrag };
